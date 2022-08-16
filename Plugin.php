3d512f605ac110cbb87c0b60e01d6d3462182b6f
Pascal Perrenoud <64425383+MrDaves24@users.noreply.github.com>
2022-08-16 18:05:50 +0200

    Using translation

--- Plugin.php
+++ Plugin.php
@@ -1,32 +1,32 @@
 <?php
-
 namespace Kanboard\Plugin\ExternalDbAuth;
 
 use Kanboard\Core\Plugin\Base;
+use Kanboard\Core\Translator;
 
 class Plugin extends Base
 {
     public function initialize() {
       // Add configuration to app panel
-      $this->template->hook->attach('template:config:application', 'ExternalDbAuth:config/infos');
+      // $this->template->hook->attach('template:config:application', 'ExternalDbAuth:config/infos');$this->template->hook->attach('template:config:sidebar', 'Calendar:config/sidebar');z
+      $this->template->hook->attach('template:config:sidebar', 'ExternalDbAuth:config/sidebar');
     }
 
-    public function getCompatibleVersion() {
+    /*public function getCompatibleVersion() {
         // Examples:
         // >=1.0.37
         // <1.0.37
         // <=1.0.37
-        return '>=1.0.0';
-    }
+        return '1.0.0';
+    }*/
 
-    public function onStartup()
-    {
-        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
+    public function onStartup() {
+      Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
     }
 
     public function getPluginVersion(){return '0.0.1';}
     public function getPluginHomepage(){return 'https://github.com/MrDaves24/kanboard-externalAuth';}
     public function getPluginAuthor(){return 'Pascal Perrenoud';}
     public function getPluginDescription(){return t('Allow user management from external DB');}
-    public function getPluginName(){return 'ExternalDbAuth';}
+    public function getPluginName(){return 'External DB Authentication';}
 }
